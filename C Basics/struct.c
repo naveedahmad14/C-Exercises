eb23473d90e011079d4cd68ea45156398a91c359
p82413sa <syed.ahmad-8@student.manchester.ac.uk>
2024-03-26 23:03:55 +0000

    struct with debugging

--- C Basics/struct.c
+++ C Basics/struct.c
@@ -16,12 +16,12 @@ timestamp add_timestamps(timestamp t1, timestamp t2) {
 
 int main(int argc, char **argv) {
     if (argc != 7) {
-        printf("Expected two timestamps.\n");
+        printf("Usage: <hour1> <minute1> <second1> <hour2> <minute2> <second2>\n");
         return 1;
     }
     timestamp t1= {atoi(argv[1]), atoi(argv[2]), atoi(argv[3])};
     timestamp t2= {atoi(argv[4]), atoi(argv[5]), atoi(argv[6])};
     timestamp sum = add_timestamps(t1, t2);
-    printf("%u %u %u\n", sum.hour, sum.minute, sum.second);
+    printf("%d %d %d\n", sum.hour, sum.minute, sum.second);
     return 0;
 }
